186ed7ef22665519c6e48433e0d615058d2837ff
Nandini Jain <nandinijain2410@gmail.com>
2021-05-08 22:46:06 +0530

    Added api call

--- src/components/Tray/SNVoice.js
+++ src/components/Tray/SNVoice.js
@@ -1,60 +1,66 @@
-import React, { useEffect, useCallback, useState } from "react";
+import React, { useEffect, useCallback, useState } from 'react';
 import './SNVoice.css';
 import SpeechRecognition, {
   useSpeechRecognition,
-} from "react-speech-recognition";
-import { SayButton } from "react-say";
-import { testapi } from './client/api'
+} from 'react-speech-recognition';
+import { SayButton } from 'react-say';
+import { translateTranscript } from './client/api';
 
 const SNVoice = () => {
-  const { transcript, resetTranscript } = useSpeechRecognition();
-  const [title, setTitle] = useState("")
+  const { transcript, resetTranscript} = useSpeechRecognition();
+  const [title, setTitle] = useState('');
+  const [translatedText,setTranslatedText] = useState('');
+
   const startListen = useCallback((event) => {
     if (event.keyCode === 32) {
       SpeechRecognition.startListening({ continuous: true });
-      setTitle("Listening...");
+      setTitle('Listening...');
     }
   }, []);
   const stopListen = useCallback((event) => {
     if (event.keyCode === 32) {
       SpeechRecognition.stopListening();
-      setTitle("");
+      setTitle('');
     }
   }, []);
   useEffect(() => {
-    document.addEventListener("keydown", startListen, false);
-    document.addEventListener("keyup", stopListen, false);
+    document.addEventListener('keydown', startListen, false);
+    document.addEventListener('keyup', stopListen, false);
 
     return () => {
-      document.removeEventListener("keydown", startListen, false);
-      document.addEventListener("keyup", stopListen, false);
+      document.removeEventListener('keydown', startListen, false);
+      document.addEventListener('keyup', stopListen, false);
     };
   }, [startListen, stopListen]);
-
 
   if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
     return null;
   }
 
-const callApi = () => {
-    testapi().then((data) =>{
+  const callApi = () => {
+    translateTranscript(transcript, 'fr').then((data) => {
       console.log(data);
-      alert(data.data.result.data);
-    })
+      alert(data.data.result.translated_text);
+      setTranslatedText(data.data.result.translated_text);
+    });
   };
 
   return (
-    <div style={{display: "flex"}}>
+    <div style={{ display: 'flex' }}>
       {title}
-      <button style={{marginRight: 10}} onClick={() => resetTranscript()}>Reset</button>
-      {/* {transcript && <SayButton
+      <button style={{ marginRight: 10 }} onClick={() => resetTranscript()}>
+        Reset
+      </button>
+      <button onClick={callApi}>Translate</button>
+      {translatedText && <SayButton
         onClick={(event) => callApi}
-        speak={transcript}
+        speak={translatedText}
       >
-       Send
-      </SayButton>} */}
-      <button onClick={callApi}>Click to Send</button>
-   <div style={{marginLeft: 10, backgroundColor: "grey"}}><p style={{color: "white"}}>{transcript}</p></div>
+       Click to send
+      </SayButton>}
+      <div style={{ marginLeft: 10, backgroundColor: 'grey' }}>
+        <p style={{ color: 'white' }}>{transcript}</p>
+      </div>
     </div>
   );
 };
--- src/components/Tray/client/api.js
+++ src/components/Tray/client/api.js
@@ -1,7 +1,10 @@
-import { getClient } from "./ClientWrapper";
+import { getClient } from './ClientWrapper';
 
-export const testapi = async ()=> {
-  const endpoint =
-    "/api/x_nuvo_sn_voice/servicenow_voice/test";
-  return await getClient().get(endpoint);
+export const translateTranscript = async (transcript, language) => {
+  const payload = {
+    target_language: 'fr',
+    source_language_message: 'Hi! How are you?',
+  };
+  const endpoint = '/api/393015/translation_service/getTranslatedText';
+  return await getClient().post(endpoint, payload);
 };
