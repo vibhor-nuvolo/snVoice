a07d16b657c3ea65e9b0d433ade1bfc17f056b93
vibhor pahare <vibhor.pahare@nuvolo.com>
2021-05-09 14:41:28 +0530

    fixed in api call

--- src/components/Tray/client/api.js
+++ src/components/Tray/client/api.js
@@ -3,7 +3,7 @@ import { getClient } from './ClientWrapper';
 export const translateTranscript = async (transcript, language) => {
   const payload = {
     target_language: 'fr',
-    source_language_message: 'Hi! How are you?',
+    source_language_message: transcript,
   };
   const endpoint = '/api/393015/translation_service/getTranslatedText';
   return await getClient().post(endpoint, payload);
